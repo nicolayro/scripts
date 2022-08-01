b175245318549048225c578c3b39ce9d55559d18
Nicolay Roness <nicroness@gmail.com>
2022-08-01 19:45:18 +0200

    Upgraded the ntnu script

--- ntnu.sh
+++ ntnu.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-CHOICE=$(find ~/ntnu -mindepth 1 -maxdepth 1 | awk -F"/" '{ print $NF }' | fzf --border )
+# Folder to search in
+FOLDER=~/ntnu
 
-cd ~/ntnu/$CHOICE
+# Find names for all the direct children in the folder
+PROJECTS=$(find $FOLDER -mindepth 1 -maxdepth 1 | awk -F "/" '{ print $NF }')
 
+# Display the list of projects and allow the user to choose
+CHOICE=$(echo "$PROJECTS" | fzf --border --margin 1,5%)
+
+# Change directory to the chosen project
+cd $FOLDER/$CHOICE
